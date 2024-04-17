cb12f3ff33b35b49fd12917ba27e3428f6e9f0d4
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-04-18 00:10:09 +0900

    Build site

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/test/" : "./",
+  base: process.env.NODE_ENV === "production" ? "/project_page/" : "./",
   plugins: [vue()],
   build: {
     outDir: "docs",
